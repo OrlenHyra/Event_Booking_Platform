429ba2edc55441750fbb65cbd7d989cc9afe5a81
Orlen Hyra <Orlen.Hyra@fti.edu.al>
2026-08-14 21:16:16 +0200

    First commit

---
+++ src/test/java/com/Final_Project/Event_Booking/EventBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Final_Project.Event_Booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

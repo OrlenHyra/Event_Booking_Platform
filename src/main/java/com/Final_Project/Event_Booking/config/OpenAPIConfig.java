10638cdc71e2a14de7afb2a8b5c53fd8183ccf48
Orlen Hyra <Orlen.Hyra@fti.edu.al>
2026-08-16 11:32:08 +0200

    add/ swagger configuration added

---
+++ src/main/java/com/Final_Project/Event_Booking/config/OpenAPIConfig.java
@@ -0,0 +1,31 @@
+package com.Final_Project.Event_Booking.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Event Booking Platform API")
+                        .version("1.0")
+                        .description("API for managing users, events and bookings"))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                "bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}

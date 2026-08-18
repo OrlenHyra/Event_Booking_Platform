e0e1fe98f9973a43879bb03b3182761f82e77ebe
Orlen Hyra <Orlen.Hyra@fti.edu.al>
2026-08-18 13:40:11 +0200

    add/bookings basic crud implementation

--- src/main/java/com/Final_Project/Event_Booking/controller/BookingController.java
+++ src/main/java/com/Final_Project/Event_Booking/controller/BookingController.java
@@ -5,7 +5,6 @@ import com.Final_Project.Event_Booking.model.dto.response.BookingResponseDTO;
 import com.Final_Project.Event_Booking.service.BookingService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;

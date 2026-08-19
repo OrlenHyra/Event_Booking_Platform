9db6d68aaa4927430dc8080431e2f9763960d854
Orlen Hyra <Orlen.Hyra@fti.edu.al>
2026-08-19 16:34:17 +0200

    add/helper method for bookings

--- src/main/java/com/Final_Project/Event_Booking/repository/BookingRepository.java
+++ src/main/java/com/Final_Project/Event_Booking/repository/BookingRepository.java
@@ -1,7 +1,9 @@
 package com.Final_Project.Event_Booking.repository;
 
 import com.Final_Project.Event_Booking.model.entity.Booking;
+import com.Final_Project.Event_Booking.model.enums.BookingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookingRepository extends JpaRepository<Booking,Long> {
+    boolean existsByBooker_IdAndEvent_IdAndStatus(Long bookerId, Long eventId, BookingStatus status);
 }
